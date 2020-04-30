953b5810043c2ab7609dd3298bbf913a80fd1e2a
bit-flipping-alejo <acaicedo@csumb.edu>
2020-04-29 17:47:44 -0700

    NACA 4 series done. Made it a Maven project

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,30 @@
+import org.knowm.xchart.QuickChart;
+import org.knowm.xchart.SwingWrapper;
+import org.knowm.xchart.XYChart;
+
 import dataContainers.AirfoilGeometry;
 
 public class Main {
 
    public static void main(String[] args) {
-      AirfoilGeometry ag = new AirfoilGeometry(2.5);
+      AirfoilGeometry ag = new AirfoilGeometry(1);
       
-      ag.becomeNACA4Series(1,2,3,4);
+      ag.becomeNACA4Series(2,4,1,2);
 
+      
+      double x[] = new double [ag.getNumberOfPoints()];
+      double y[] = new double [ag.getNumberOfPoints()];
+      double thePts[][] = ag.getPoints();
+      
+      for (int i = 0; i < ag.getNumberOfPoints(); i++) {
+         x[i] = thePts[i][0];
+         y[i] = thePts[i][1];
+      }
+      
+      XYChart chart = QuickChart.getChart("your airfoil", "cord location", "thickness", "airfoil points", x, y);
+      new SwingWrapper(chart).displayChart();
+      
+      
    }
 
 }
--- src/dataContainers/AirfoilGeometry.java
+++ src/dataContainers/AirfoilGeometry.java
@@ -56,28 +56,68 @@ public class AirfoilGeometry {
    /*Naca 4 + 5 Series eqns*/
    public void becomeNACA4Series(int nacaNumber1, int nacaNumber2, int nacaNumber3, int nacaNumber4 ) {
       //fill X coord with cos spaced points
+      //https://en.wikipedia.org/wiki/NACA_airfoil#Equation_for_a_symmetrical_4-digit_NACA_airfoil
+      //http://www.aerospaceweb.org/question/airfoils/q0100.shtml
+      //http://www.aerospaceweb.org/question/airfoils/q0041.shtml
       this.generateCosSpacing();
       
-      double m = (double) nacaNumber1;
+      double m = (double) nacaNumber1 / 100.0;
+      double t = (nacaNumber3 * 10) + nacaNumber4;
+      t = t / 100.0;
+      
       //p is location of max camber
-      double p =  this.cordLength * ((double) nacaNumber2) / 10.0 ;
+      double p =  ((double) nacaNumber2) / 10.0 ;
+      double yc = 0;
       double yt = 0;
       double x_over_c = 0;
+      double dy_dx = 0;
+      double delineationBtnTopAndBtm = Math.floor((this.numberOfPoints ) / 2);
+      double theta = 0;
       
       for (int i = 0; i < this.numberOfPoints; i++) {
          
-         x_over_c = (this.points[i][1]/this.cordLength);
-         if (this.points[i][1] < p ) {
+         x_over_c = (this.points[i][0]/this.cordLength);
+         
+         yt = 5.0 * t * ( 0.2969*Math.sqrt(x_over_c) - 0.126*x_over_c 
+               - 0.3516*Math.pow(x_over_c, 2) + 0.2843*Math.pow(x_over_c, 3) 
+               - 0.1015*Math.pow(x_over_c, 4) );
+               
+         if (this.points[i][1] < this.cordLength * p ) {
             // less than point of max camber                        
-            yt = (m/ Math.pow(p, 2)) * (2*p*x_over_c - Math.pow(x_over_c, 2) );
+            yc = (m/ Math.pow(p, 2)) * (2*p*x_over_c - Math.pow(x_over_c, 2) );         
+            dy_dx = ( (2 * m) / Math.pow(p, 2) ) * (p - x_over_c);   
+            
          } else {
             // greater than point of max camber
-            yt = ( m / Math.pow(( 1 - p ), 2) ) * (p - x_over_c);
+            yc = ( m / Math.pow(( 1 - p ), 2) ) * ( (1 - 2*p) + 2*p*x_over_c - Math.pow(x_over_c,  2)   );     
+            dy_dx = ( (2 * m) / Math.pow((1 - p), 2) ) * (p - x_over_c);
+            
+         }
+         
+         theta = Math.atan(dy_dx);
+         
+         if (i < delineationBtnTopAndBtm) {
+            //xUpper
+            this.points[i][0] = this.points[i][0] - yt * Math.sin(theta);             
+            //yUpper
+            this.points[i][1] = yc + yt * Math.cos(theta);
+            
+            
+         } else {
+            // xLower
+            this.points[i][0] = this.points[i][0] + yt * Math.sin(theta);             
+            //yLower
+            this.points[i][1] = yc - yt * Math.cos(theta);            
+            
          }
          
          
          
       }
+      
+      // Ensure midpoints are equal for closed airfoil
+      int midPt = (int) Math.floor((this.numberOfPoints ) / 2);      
+      this.points[midPt + 1][1] = this.points[midPt][1 ];
       
       
    }
@@ -111,16 +151,17 @@ public class AirfoilGeometry {
       
       
       double currAngle = deltaAngle;
+      
       for (int i = 0 ; i < numPtsOnX; i++) {         
          currAngle = deltaAngle * i; 
-         this.points[i][1] = midpt * (1 - Math.cos(currAngle));
-         this.points[ this.numberOfPoints - (i + 1) ][1] = this.points[i][1];
+         this.points[i][0] = midpt * (1 - Math.cos(currAngle));
+         this.points[ this.numberOfPoints - (i + 1) ][0] = this.points[i][0];
                  
       }
-      
+      /*
       for (int i = 0; i < this.numberOfPoints; i++) {
          System.out.println("Point: " + i + ", value: " + this.points[i][1] + ", currAngle: " + currAngle );
-      }
+      }*/
       
    }
    
