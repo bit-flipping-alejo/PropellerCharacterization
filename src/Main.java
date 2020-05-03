2878a3a0c147fbdfbe86adba4ef7132f5ff858b7
bit-flipping-alejo <acaicedo@csumb.edu>
2020-05-03 15:37:31 -0700

    started matrix solver class, impl gauss elim, on testing in the main class

--- src/Main.java
+++ src/Main.java
@@ -11,46 +11,74 @@ import geometryContainers.AirfoilGeometry;
 public class Main {
 
    public static void main(String[] args) {
+      //showAirfoil();
+      
+      testAGaussElim();
+      
+
+   }
+
+   public static void showAirfoil() {
       AirfoilGeometry ag = new AirfoilGeometry(2.5);
-      
+
       ag.setangleOfAttackRad(5 * (Math.PI/180));
-      
+
       ag.becomeNACA4Series(2,4,1,2);
       ag.generateControlPoints();
-      
-      
+
+
       double x[] = new double [ag.getNumberOfPoints()];
       double y[] = new double [ag.getNumberOfPoints()];
-      
+
       double xc[] = new double [ag.getNumberOfPoints() - 1];
       double yc[] = new double [ag.getNumberOfPoints() - 1];
-      
+
       double thePts[][] = ag.getPoints();
       double ctrlPts[][] = ag.getControlPoints();
-      
+
       for (int i = 0; i < ag.getNumberOfPoints(); i++) {
          x[i] = thePts[i][0];
          y[i] = thePts[i][1];
-         
+
          if (i != ag.getNumberOfPoints() - 1) {
             xc[i] = ctrlPts[i][0];
             yc[i] = ctrlPts[i][1];
          } 
-         
+
          /*
          if (i > ag.getNumberOfPoints() / 4) {
             break;
          }
-           */ 
+          */ 
       }
       //https://knowm.org/javadocs/xchart/index.html
       XYChart chart = QuickChart.getChart("your airfoil", "cord location", "thickness", "airfoil points", x, y);      
       XYSeries series = chart.addSeries("ControlPts", xc, yc);
       series.setMarker(SeriesMarkers.DIAMOND);
-      
+
       new SwingWrapper(chart).displayChart();
+   }
+
+   public static void testAGaussElim() {
+      double[][] testA = new double[3][3];
+      double[] testB = new double [3];
+      
+      testA[0][1] = 2;
+      testA[0][2] = 1;
+      testA[0][3] = -1;
+      
+      testA[1][1] = -3;
+      testA[1][2] = -1;
+      testA[1][3] = 2;
+      
+      testA[2][1] = -2;
+      testA[2][2] = 1;
+      testA[2][3] = 2;
+      
+      testB[0] = 8;
+      testB[1] = -11;
+      testB[2] = -3;
       
       
    }
-
 }
---
+++ src/solvers/MatrixSolver.java
@@ -0,0 +1,189 @@
+package solvers;
+
+public class MatrixSolver {
+
+   /* Pseudo code at bottom of page
+    * https://en.wikipedia.org/wiki/Gaussian_elimination
+    * https://en.wikipedia.org/wiki/LU_decomposition
+    * */
+
+   // of form AX = B, you solve for x
+   private double [][] A;
+   private double [] X;
+   private double [] B;
+
+   private double[][] augmentedMatrix;
+   
+   private int numRows;
+   private int numCols;
+
+   public MatrixSolver(double[][] a, double[] b, int numRows, int numCols) {
+      super();
+      A = a;
+      B = b;
+      this.numRows = numRows;
+      this.numCols = numCols;
+      
+      this.augmentedMatrix = new double[numRows + 1][numCols]; 
+      this.populateAugmentedMatrix();
+   }
+
+
+
+   /*Gaussian elimination chosen since each Geometric
+    * integral is calculated per point O(n^2) where as
+    * LU Decomposition is performed in O(n^3) but it allows
+    * solutions in O(N^2)*/
+
+   public void doGaussianElimination() {
+      
+      int h = 0; 
+      int k = 0;
+      
+      while ( (h < this.numRows) && (k < this.numCols) ) {
+         
+         //identify max row in the current col
+         //store off as maxRowNotH
+         int maxRowNotH = h;
+         for(int rowctr = h; rowctr < this.numRows; rowctr++) {            
+            if (this.augmentedMatrix[rowctr][k] > this.augmentedMatrix[maxRowNotH][k] ) {
+               maxRowNotH = rowctr;
+            }            
+         }
+         
+         if ( this.augmentedMatrix[maxRowNotH][k] == 0 ) {
+            k++;
+         } else {
+            // do swap
+            this.swapAugmentedRows(h, maxRowNotH);
+            
+            for (int i = h + 1; i < this.numRows; i++) {
+               double ratio = this.augmentedMatrix[i][k] / this.augmentedMatrix[h][k];
+               this.augmentedMatrix[i][k] = 0;
+               
+               for (int j = k + 1; j < this.numCols; j++) {
+                  this.augmentedMatrix[i][j] = this.augmentedMatrix[i][j] - this.augmentedMatrix[h][j] * ratio;
+               }
+               
+            }
+            
+         } // end if          
+      } // end while
+      
+      
+   }
+
+   private void swapAugmentedRows(int row1, int row2) {
+      
+      for (int i = 0; i < this.numCols + 1; i++) {
+         double tempVal = this.A[row1][i];
+         this.A[row1][i] = this.A[row2][i];
+         this.A[row2][i] = tempVal;
+      }
+      
+   }
+
+   //Augmented matrix is regular A matrix, 
+   //but with n+1 columns, in the n+1th column 
+   //the B matrix resides, this allows all 
+   // elementary operations to be performed
+   // at once
+   private void populateAugmentedMatrix() {      
+      for (int i = 0; i < this.numRows; i++) {      
+         for (int j = 0; j < this.numCols + 1; j++) {
+            if(j == this.numRows) {
+               this.augmentedMatrix[i][j] = this.B[i];
+            } else {
+               this.augmentedMatrix[i][j] = this.A[i][j];
+            }            
+         } // end j         
+      } // end i      
+   }
+
+
+
+   /* * * * * * * * * * * * * * * 
+    * 
+    * Solution provided by letting A
+    * equal to LU where L is lower 
+    * triangular and U is upper 
+    * triangular
+    * 
+    * reframes problem as LUX = B
+    * with a dummy variable substitution
+    * of UX = Y you get LY = B , then you can
+    * solve UX = Y for X which breaks it into 
+    * 2 smaller problems instead of 1 bigger one
+    * 
+    * * * * * * * * * * * * * * */
+
+   
+   
+   
+   
+   
+   
+   /*Getters and setters*/
+   
+   
+   public double[][] getA() {
+      return A;
+   }
+
+
+
+   public void setA(double[][] a) {
+      A = a;
+   }
+
+
+
+   public double[] getB() {
+      return B;
+   }
+
+
+
+   public void setB(double[] b) {
+      B = b;
+   }
+
+
+
+   public int getNumRows() {
+      return numRows;
+   }
+
+
+
+   public void setNumRows(int numRows) {
+      this.numRows = numRows;
+   }
+
+
+
+   public int getNumCols() {
+      return numCols;
+   }
+
+
+
+   public void setNumCols(int numCols) {
+      this.numCols = numCols;
+   }
+
+
+
+   public double[] getX() {
+      return X;
+   }
+
+
+
+   public double[][] getAugmentedMatrix() {
+      return augmentedMatrix;
+   }
+   
+   
+}
+
